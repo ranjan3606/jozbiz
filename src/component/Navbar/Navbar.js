43157416314c74e45e084e72654d43121adfc4b3
ranjan3606 <ranjan17@navgurukul.org>
2022-03-20 21:58:17 +0530

    static design

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -28,9 +28,11 @@ function Navbar() {
                                 <a className="nav-link" aria-current="page" href="/">Sale</a>
                             </li>
                             <li className="nav-item">
-                                <div className='login'><a className="nav-link btn text-dark" aria-current="page" href="/">Log In <svg className='svg' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+                                <div className='login'>
+                                    <a className="nav-link btn text-dark" aria-current="page" href="/">Log In <svg className='svg' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                     <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
-                                </svg></a></div>
+                                </svg></a>
+                                </div>
                             </li>
                         </ul>
                     </div>
